38343fc46a382e2903f968f8c35c1b4d9b6dd32f
Godswill udemba <godswill.udemba.88009@unn.edu.ng>
2022-08-07 20:36:19 +0100

    first changes

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ document.getElementById("demo").innerHTML = 5 + 6;
 
 //using docc.write
 document.write(6 + 5);
+
+/*also note that console.log() is used is isplaying data and
+for debuging purposes*/
+
+console.log(5 + 6);
+
+//JAVA SCRIPT STATEMENT
+
+let x, y, z;
+x = 6;
+y = 6;
+z = x + y;
+console.log(z);
+
+document.getElementById("demo2").innerHTML = "hello dolly";
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "hello Sammy";
+  document.getElementById("demo2").innerHTML = "God bless you!";
+}
